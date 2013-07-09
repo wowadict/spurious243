ef871fa0963323e6ed051b9cf71471d1019f12c2
Evangeline2013 <mfrink@Baldwin-telecom.net>
2013-07-09 11:44:28 -0500

    Moving ALL SQLs to Database Repository. https://github.com/mangosvb/databaseOne

--- Developmental/SQL/updates/update_life.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `creatures` ADD `creature_minlife` INT DEFAULT '0' NOT NULL AFTER `creature_size`;
-ALTER TABLE `creatures` ADD `creature_maxlife` INT DEFAULT '0' NOT NULL AFTER `creature_minlife`;
-ALTER TABLE `creatures` ADD `creature_minmana` INT DEFAULT '0' NOT NULL AFTER `creature_maxlife`;
-ALTER TABLE `creatures` ADD `creature_maxmana` INT DEFAULT '0' NOT NULL AFTER `creature_minlife`;
